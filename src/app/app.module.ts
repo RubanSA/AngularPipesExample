1eab5226657c20810fec12908e1da4a7dbf41871
Сергей Рубан <ruban@kr.step>
2020-11-24 19:25:00 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PipesTestComponent } from './pipes-test/pipes-test.component';
+import { JoinPipe } from './join.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesTestComponent,
+    JoinPipe
   ],
   imports: [
     BrowserModule
