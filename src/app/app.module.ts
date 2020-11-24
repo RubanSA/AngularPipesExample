5847db0663d87f3af41d02dcd97b2cfac9162e84
Сергей Рубан <ruban@kr.step>
2020-11-24 21:08:42 +0200

    some additional components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PipesTestComponent } from './pipes-test/pipes-test.component';
 import { JoinPipe } from './join.pipe';
+import {WeatherInfoComponent} from "../app/weather-info/weather-info.component"
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     PipesTestComponent,
+    WeatherInfoComponent,
     JoinPipe
   ],
   imports: [
